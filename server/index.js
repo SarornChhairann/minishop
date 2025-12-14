3892b6dd54ffc3b8766ded566e5cafa1ddf9722f
Chhaiann <chhaiann@Chhaianns-MacBook-Pro.local>
2025-12-14 16:01:04 +0700

    fix ssl mode pg

--- server/index.js
+++ server/index.js
@@ -56,6 +56,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: true
 });
 
 // Test database connection
